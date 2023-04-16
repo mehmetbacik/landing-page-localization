fda3f2e4543da7aea199636805ff5d0eabbdab8e
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-04-16 20:55:49 +0300

    Update
    Style and content updated.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {IntlProvider, FormattedMessage} from 'react-intl'
 
 const messages = {
   'tr-TR': {
-    title: "Başlık",
-    description: "Açıklama",
+    title: "Hayranlarınızın Seveceği Topluluğu Oluşturun",
+    description: "Huddle, topluluk oluşturma yöntemimizi yeniden tasarlıyor. Bir sesiniz var, ancak izleyicilerinizin de var. Gerçek bir tartışmaya girerken kullanıcılarınızla bağlantılar oluşturun.",
+    button: "Kayıt Ol",
   },
   'en-US': {
-    title: "Title",
-    description: "Description",
+    title: "Build The Community Your Fans Will Love",
+    description: "Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.",
+    button: "Register",
   }
 }
 
@@ -22,15 +24,26 @@ function App() {
   }, [locale])
   return (
     <div className="App">
-      <IntlProvider locale={locale} messages={messages[locale]}>
-        <FormattedMessage id="title" />
-        <p>
-          <FormattedMessage id="description" />
-        </p>
-        <br /><br />
-        <button onClick={() => setLocale('tr-TR')}>TR</button>
-        <button onClick={() => setLocale('en-US')}>EN</button>
-      </IntlProvider>
+      <div className='container mx-auto px-2'>
+        <div className='header flex justify-between items-center py-10'>
+          <div className='logo'>
+            <img src='images/logo.svg' alt='Logo' />
+          </div>
+          <div className='language'>
+            <button onClick={() => setLocale('tr-TR')}>TR</button>
+            <button onClick={() => setLocale('en-US')}>EN</button>
+          </div>
+        </div>
+        <IntlProvider locale={locale} messages={messages[locale]}>
+          <FormattedMessage id="title" />
+          <p>
+            <FormattedMessage id="description" />
+          </p>
+          <FormattedMessage id="button" />
+          <br /><br />
+
+        </IntlProvider>
+      </div>
     </div>
   );
 }
