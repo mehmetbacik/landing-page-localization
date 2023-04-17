246d20e3809d737be14884aa612c3a5e980e6adb
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-04-18 00:31:54 +0300

    Update
    Index updated for style

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   }, [locale])
   return (
     <div className="App">
-      <div className='container mx-auto px-2'>
+      <div className='container mx-auto px-8 md:px-10 lg:px-2'>
         <div className='header flex justify-between items-center py-12'>
           <div className='logo'>
             <img src='images/logo.svg' alt='Logo' />
@@ -34,26 +34,28 @@ function App() {
             <button onClick={() => setLocale('en-US')}><img src='images/en.png' alt='en' /></button>
           </div>
         </div>
-        <div className='main grid items-center grid-cols-2 gap-10 pt-8'>
+        <div className='main grid items-center grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-10 pt-0 md:pt-8 lg:pt-8'>
           <div className='images'>
             <img src='images/illustration-mockups.svg' alt='img' />
           </div>
           <div className='content mb-12'>
             <IntlProvider locale={locale} messages={messages[locale]}>
-              <div className='headline text-5xl leading-normal font-bold text-white'>
+              <div className='headline leading-normal md:leading-normal lg:leading-normal text-3xl md:text-3xl lg:text-5xl text-center md:text-center lg:text-left font-bold text-white'>
                 <FormattedMessage id="title" />
               </div>
-              <div className='text text-xl leading-normal text-white py-5'>
+              <div className='text text-lg md:text-lg lg:text-lg text-center md:text-center lg:text-left leading-normal text-white py-5'>
                 <FormattedMessage id="description" />
               </div>
-              <button className='btn tracking-wider text-lg'>
-                <FormattedMessage id="button" />
-              </button>
+              <div className='main-btn flex justify-center md:justify-center lg:justify-start'>
+                <button className='btn tracking-wider text-sm	md:text-lg lg:text-lg'>
+                  <FormattedMessage id="button" />
+                </button>
+              </div>
             </IntlProvider>
           </div>
         </div>
-        <div className='footer pt-4'>
-          <div className='social flex justify-end items-center gap-2'>
+        <div className='footer pt-0 md:pt-4 lg:pt-4 pb-4 md:pb-4 lg:pb-0'>
+          <div className='social flex justify-center md:justify-center lg:justify-end items-center gap-2'>
             <img src='images/fb.png' alt='Logo' />
             <img src='images/tw.png' alt='Logo' />
             <img src='images/in.png' alt='Logo' />
